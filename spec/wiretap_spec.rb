6a39b4803e57fb4c95d13fcac7b647b19fa0f66c
James Wozniak <wozza35@hotmail.com>
2025-11-26 02:38:05 +0000

    Initial commit

---
+++ spec/wiretap_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Wiretap do
+  it "has a version number" do
+    expect(Wiretap::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
