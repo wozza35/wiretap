f29dce2d52a16347f708369979287f58560fefcb
James Wozniak <wozza35@hotmail.com>
2025-11-28 03:09:12 +0000

    Adds Wiretap.monitor class method

--- lib/wiretap.rb
+++ lib/wiretap.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "wiretap/version"
+require_relative "wiretap/proxy"
 
 module Wiretap
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.monitor(target)
+    Proxy.new(target)
+  end
 end
--- spec/wiretap_spec.rb
+++ spec/wiretap_spec.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+require 'wiretap/proxy'
+
 RSpec.describe Wiretap do
   it "has a version number" do
     expect(Wiretap::VERSION).not_to be nil
+  end
+
+  describe ".monitor" do
+    it "returns a Proxy wrapping the target object" do
+      target = Object.new
+      expect(Wiretap::Proxy).to receive(:new).with(target).and_return(proxy = double)
+      expect(Wiretap.monitor(target)).to be(proxy)
+    end
   end
 end
